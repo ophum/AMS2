a810ac6a959a378e6e0232304a9353479b3698d5
ophum <inagaki0106@gmail.com>
2018-12-28 09:57:04 +0900

    [change] config.phpに実装していた関数をlib.phpに移行

--- ID/config.php
+++ ID/config.php
@@ -9,31 +9,3 @@ define('DB', 'http://IPAddress or host/DB/');
 
 define('DBID', "");
 define('DBPASS', "");
-
-function h($s){
-	return htmlspecialchars(trim($s), ENT_QUOTES, 'utf-8');
-}
-
-function post($addr, $data){
-
-	$content = http_build_query($data);
-	$headers = implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',));
-	$options = array(
-		'http' => array(
-			'method' => 'POST',
-			'content' => $content,
-			'header' => $headers
-	));
-
-    $ret = json_decode(file_get_contents($addr, false, stream_context_create($options)), true);
-    
-    switch($ret['type']){
-    case 'bool':
-        if($ret['data'] == 'true') return true;
-        else return false;
-    case 'string':
-        return $ret['data'];
-    case 'number':
-        return intval($ret['data']);
-    }
-}
